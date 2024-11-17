4b26ea670af2e6f3d58714fcf8c24f4e326012fb
heasheartfire <gagi1995@gmail.com>
2024-11-17 01:30:16 +0100

    fix(Api): Update controller to use IDbContextFactory

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,28 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookHeaven.Domain;
+using Microsoft.AspNetCore.Mvc;
 using BookHeaven.Domain.Entities;
 using BookHeaven.Domain.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookHeaven.Server.Controllers
 {
 	[ApiController]
 	[Route("api")]
-	public class ApiController(IDatabaseService DatabaseService, ILogger<ApiController> logger) : Controller
+	public class ApiController(IDbContextFactory<DatabaseContext> dbContextFactory, ILogger<ApiController> logger) : Controller
 	{
 
 		[HttpGet("ping")]
 		public ActionResult Ping() => Ok();
 
 		[HttpGet("books")]
-		public async Task<ActionResult> GetAll() => new JsonResult(await DatabaseService.GetAllIncluding<Book>(b => b.Author, b => b.Series));
+		public async Task<ActionResult> GetAll()
+		{
+			await using var context = await dbContextFactory.CreateDbContextAsync();
+			
+			return new JsonResult(await context.Books.Include(b => b.Author).Include(b => b.Series).ToListAsync());
+		}
 
 		[HttpGet("authors")]
-		public async Task<ActionResult> GetAllAuthors() => new JsonResult(await DatabaseService.GetAll<Author>());
+		public async Task<ActionResult> GetAllAuthors()
+		{
+			await using var context = await dbContextFactory.CreateDbContextAsync();
+			
+			return new JsonResult(await context.Authors.ToListAsync());
+		}
 
 		[HttpGet("profiles")]
-		public async Task<ActionResult> GetAllProfiles() => new JsonResult(await DatabaseService.GetAll<Profile>());
+		public async Task<ActionResult> GetAllProfiles()
+		{
+			await using var context = await dbContextFactory.CreateDbContextAsync();
+			
+			return new JsonResult(await context.Profiles.ToListAsync());
+		}
 
 		[HttpGet("profiles/{profileId}/{bookId}")]
-		public async Task<ActionResult> GetBookProgress(Guid bookId, Guid profileId) => new JsonResult(await DatabaseService.GetBy<BookProgress>(p => p.ProfileId == profileId && p.BookId == bookId));
+		public async Task<ActionResult> GetBookProgress(Guid bookId, Guid profileId)
+		{
+			await using var context = await dbContextFactory.CreateDbContextAsync();
+			
+			return new JsonResult(await context.BooksProgress.FirstOrDefaultAsync(x => x.BookId == bookId && x.ProfileId == profileId));
+		}
 
 		[HttpPost("progress/update")]
 		public async Task<ActionResult> UpdateBookProgress([FromBody] BookProgress progress)
@@ -30,12 +52,14 @@ namespace BookHeaven.Server.Controllers
 			logger.LogInformation($"Received progress update for book {progress.BookProgressId} (LastRead: {progress.LastRead})");
 			try
 			{
-				BookProgress? existing = await DatabaseService.Get<BookProgress>(progress.BookProgressId);
-				if (existing != null && progress.LastRead <= existing.LastRead) return Ok();
+				await using var context = await dbContextFactory.CreateDbContextAsync();
+				
+				var existingProgress = await context.BooksProgress.FirstOrDefaultAsync(x => x.BookProgressId == progress.BookProgressId);
+				if (existingProgress != null && progress.LastRead <= existingProgress.LastRead) return Ok();
 				
 				logger.LogInformation("Updating progress");
-				await DatabaseService.AddOrUpdate(progress);
-				await DatabaseService.SaveChanges();
+				context.BooksProgress.Update(progress);
+				await context.SaveChangesAsync();
 				return Ok();
 			}
 			catch (Exception ex)
