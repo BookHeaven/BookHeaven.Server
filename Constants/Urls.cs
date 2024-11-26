0e3db13f8afdba51a14515355d38dac3b2f8a48e
heasheartfire <gagi1995@gmail.com>
2024-11-26 15:47:11 +0100

    refactor(urls): Fully switch to constants

--- Constants/Urls.cs
+++ Constants/Urls.cs
@@ -5,6 +5,7 @@ public static class Urls
 	public const string Shelf = "/shelf";
 	public const string Authors = "/authors";
 	public const string Series = "/series";
+	public const string Settings = "/settings";
 
 	public static string GetBookUrl(Guid bookId)
 	{
