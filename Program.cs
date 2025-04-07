cf12cba719811f741087370d9bd3efd55e763b74
heasheartfire <gagi1995@gmail.com>
2025-04-07 23:44:44 +0200

    feat: Add broadcasting capabilities to avoi writing the server's url on the client

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using EpubManager;
 using EpubManager.Entities;
 using Microsoft.AspNetCore.StaticFiles;
@@ -16,143 +15,146 @@ using BookHeaven.Server.Abstractions;
 using BookHeaven.Server.Endpoints;
 using BookHeaven.Server.Extensions;
 using MediatR;
-using Microsoft.AspNetCore.Localization;
-using DependencyInjection = BookHeaven.Domain.DependencyInjection;
 
-namespace BookHeaven.Server
+namespace BookHeaven.Server;
+
+public class Program
 {
-	public class Program
-	{
-		private static readonly string AppDataPath = Path.Combine(Directory.GetCurrentDirectory(), "data");
-		public static readonly string ImportPath = Path.Combine(Directory.GetCurrentDirectory(), "import");
-		public static readonly string BooksPath = Path.Combine(AppDataPath, "books");
-		public static readonly string CoversPath = Path.Combine(AppDataPath, "covers");
-		public static readonly string DatabasePath = Path.Combine(AppDataPath, "database");
-		public static readonly string FontsPath = Path.Combine(AppDataPath, "fonts");
-		public static Profile? SelectedProfile { get; set; }
+	private static readonly string AppDataPath = Path.Combine(Directory.GetCurrentDirectory(), "data");
+	public static readonly string ImportPath = Path.Combine(Directory.GetCurrentDirectory(), "import");
+	public static readonly string BooksPath = Path.Combine(AppDataPath, "books");
+	public static readonly string CoversPath = Path.Combine(AppDataPath, "covers");
+	public static readonly string DatabasePath = Path.Combine(AppDataPath, "database");
+	public static readonly string FontsPath = Path.Combine(AppDataPath, "fonts");
+	public static Profile? SelectedProfile { get; set; }
 		
-		public static void Main(string[] args)
+	public static async Task Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+			
+		builder.Services.AddLocalization(options => options.ResourcesPath = "Localization");
+			
+		Directory.CreateDirectory(BooksPath);
+		Directory.CreateDirectory(CoversPath);
+		Directory.CreateDirectory(DatabasePath);
+		Directory.CreateDirectory(FontsPath);
+		
+		builder.Services.AddRazorComponents()
+			.AddInteractiveServerComponents();
+		builder.Services.AddControllers().AddJsonOptions(x =>
 		{
-			var builder = WebApplication.CreateBuilder(args);
+			x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+		});
+
+		builder.Services.AddMudServices(config =>
+		{
+			config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+			config.SnackbarConfiguration.HideTransitionDuration = 500;
+			config.SnackbarConfiguration.ShowTransitionDuration = 500;
+			config.SnackbarConfiguration.PreventDuplicates = false;
+		});
+
+		builder.Services.AddDomain(DatabasePath);
+		builder.Services.AddEpubManager();
 			
-			builder.Services.AddLocalization(options => options.ResourcesPath = "Localization");
+		builder.Services.AddScoped<ISettingsManagerService, SettingsManagerService>();
+		builder.Services.AddScoped<IMetadataProviderService, GoogleBooksService>();
+		// builder.Services.AddScoped<IMetadataProviderService, OpenLibraryService>();
+		builder.Services.AddScoped<IFormatService<EpubBook>, EpubService>();
+
+		builder.Services.AddSingleton<UdpBroadcastServer>();
 			
-			Directory.CreateDirectory(BooksPath);
-			Directory.CreateDirectory(CoversPath);
-			Directory.CreateDirectory(DatabasePath);
-			Directory.CreateDirectory(FontsPath);
+		// Add endpoints
+		builder.Services.AddEndpoints(typeof(Program).Assembly);
 
-			// Add services to the container.
-			builder.Services.AddRazorComponents()
-				.AddInteractiveServerComponents();
-			builder.Services.AddControllers().AddJsonOptions(x =>
-			{
-				x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-			});
-
-			builder.Services.AddMudServices(config =>
-			{
-				config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
-				config.SnackbarConfiguration.HideTransitionDuration = 500;
-				config.SnackbarConfiguration.ShowTransitionDuration = 500;
-				config.SnackbarConfiguration.PreventDuplicates = false;
-			});
-
-			builder.Services.AddDomain(DatabasePath);
+		var app = builder.Build();
 			
-			builder.Services.AddEpubManager();
-			builder.Services.AddScoped<ISettingsManagerService, SettingsManagerService>();
-			builder.Services.AddScoped<IMetadataProviderService, GoogleBooksService>();
-			// builder.Services.AddScoped<IMetadataProviderService, OpenLibraryService>();
-			builder.Services.AddScoped<IFormatService<EpubBook>, EpubService>();
+		var supportedCultures = new[]{ "en-US", "es-ES" };
+		var localizationOptions = new RequestLocalizationOptions()
+			.SetDefaultCulture(supportedCultures[0])
+			.AddSupportedCultures(supportedCultures)
+			.AddSupportedUICultures(supportedCultures);
 			
-			// Add endpoints
-			builder.Services.AddEndpoints(typeof(Program).Assembly);
+		app.UseRequestLocalization(localizationOptions);
 
-			var app = builder.Build();
-			
-			var supportedCultures = new[]{ "en-US", "es-ES" };
-			var localizationOptions = new RequestLocalizationOptions()
-				.SetDefaultCulture(supportedCultures[0])
-				.AddSupportedCultures(supportedCultures)
-				.AddSupportedUICultures(supportedCultures);
-			
-			app.UseRequestLocalization(localizationOptions);
-
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-				app.UseHttpsRedirection();
-			}
-
-			FileExtensionContentTypeProvider provider = new()
-			{
-				Mappings =
-				{
-					[".epub"] = "application/epub+zip"
-				}
-			};
-
-			app.MapStaticAssets();
-			app.UseStaticFiles(new StaticFileOptions
-			{
-				FileProvider = new PhysicalFileProvider(AppDataPath),
-				ContentTypeProvider = provider
-			});
-			
-            app.UseAntiforgery();
-
-			app.MapRazorComponents<App>()
-				.AddInteractiveServerRenderMode();
-
-			using (var scope = app.Services.CreateScope())
-			{
-				// Create default profile if it doesn't exist
-				Profile defaultProfile;
-				var sender = scope.ServiceProvider.GetRequiredService<ISender>();
-
-				var profileQuery = Task.Run(async() => await sender.Send(new GetDefaultProfileQuery())).Result;
-				if(profileQuery.IsFailure)
-				{
-					var createProfile = Task.Run(async() => await sender.Send(new CreateProfileCommand("Default"))).Result;
-					if (createProfile.IsFailure)
-					{
-						throw new Exception(createProfile.Error.Description);
-					}
-					defaultProfile = createProfile.Value;
-				}
-				else
-				{
-					defaultProfile = profileQuery.Value;
-				}
-				
-				// To be changed when the user is able to pick a profile
-				SelectedProfile = defaultProfile;
-			}
-			//app.MapControllers();
-
-			app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/")
-                {
-                    context.Response.Redirect("/shelf");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-			
-			app.MapCultureEndpoint();
-			
-			var mapGroup = app.MapGroup("/api");
-			mapGroup.MapGet("ping", () => Results.Ok());
-			app.MapEndpoints(mapGroup);
-
-			app.Run();
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+			app.UseHttpsRedirection();
 		}
+
+		FileExtensionContentTypeProvider provider = new()
+		{
+			Mappings =
+			{
+				[".epub"] = "application/epub+zip"
+			}
+		};
+
+		app.MapStaticAssets();
+		app.UseStaticFiles(new StaticFileOptions
+		{
+			FileProvider = new PhysicalFileProvider(AppDataPath),
+			ContentTypeProvider = provider
+		});
+			
+		app.UseAntiforgery();
+
+		app.MapRazorComponents<App>()
+			.AddInteractiveServerRenderMode();
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var udpBroadcastServer = scope.ServiceProvider.GetRequiredService<UdpBroadcastServer>();
+			_ = udpBroadcastServer.StartAsync();
+		}
+
+		using (var scope = app.Services.CreateScope())
+		{
+			// Create default profile if it doesn't exist
+			Profile defaultProfile;
+			var sender = scope.ServiceProvider.GetRequiredService<ISender>();
+
+			var profileQuery = await sender.Send(new GetDefaultProfileQuery());
+			if(profileQuery.IsFailure)
+			{
+				var createProfile = await sender.Send(new CreateProfileCommand("Default"));
+				if (createProfile.IsFailure)
+				{
+					throw new Exception(createProfile.Error.Description);
+				}
+				defaultProfile = createProfile.Value;
+			}
+			else
+			{
+				defaultProfile = profileQuery.Value;
+			}
+				
+			// To be changed when the user is able to pick a profile
+			SelectedProfile = defaultProfile;
+		}
+
+		app.Use(async (context, next) =>
+		{
+			if (context.Request.Path == "/")
+			{
+				context.Response.Redirect("/shelf");
+			}
+			else
+			{
+				await next();
+			}
+		});
+			
+		app.MapCultureEndpoint();
+			
+		var mapGroup = app.MapGroup("/api");
+		mapGroup.MapGet("ping", () => Results.Ok());
+		app.MapEndpoints(mapGroup);
+
+		await app.RunAsync();
 	}
 }
---
+++ Services/UdpBroadcastServer.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using BookHeaven.Domain.Constants;
+
+namespace BookHeaven.Server.Services;
+
+public class UdpBroadcastServer(ILogger<UdpBroadcastServer> logger)
+{
+    private readonly string _message = $"{Broadcast.SERVER_URL_MESSAGE_PREFIX}{Environment.GetEnvironmentVariable("SERVER_URL")}";
+
+    public async Task StartAsync()
+    {
+        if (Environment.GetEnvironmentVariable("SERVER_URL") == null)
+        {
+            logger.LogError("SERVER_URL environment variable is not set, can't start broadcast.");
+            return;
+        }
+        
+        using var udpClient = new UdpClient(Broadcast.BROADCAST_PORT);
+        logger.LogInformation("Waiting for clients...");
+
+        while (true)
+        {
+            var result = await udpClient.ReceiveAsync();
+            var receivedMessage = Encoding.UTF8.GetString(result.Buffer);
+
+            if (receivedMessage != Broadcast.DISCOVER_MESSAGE)
+            {
+                await Task.Delay(2000);
+                continue;
+            }
+            
+            logger.LogInformation($"Received discover broadcast from {result.RemoteEndPoint.Address}:{result.RemoteEndPoint.Port}");
+            var broadcastAddress = new IPEndPoint(IPAddress.Broadcast, Broadcast.BROADCAST_PORT);
+            var messageBytes = Encoding.UTF8.GetBytes(_message);
+
+            using var responseClient = new UdpClient
+            {
+                EnableBroadcast = true
+            };
+
+            var acknowledged = false;
+            while (!acknowledged)
+            {
+                await responseClient.SendAsync(messageBytes, messageBytes.Length, broadcastAddress);
+                logger.LogInformation($"Broadcasted message {_message}");
+
+                udpClient.Client.ReceiveTimeout = 2000;
+                try
+                {
+                    var ackResult = await udpClient.ReceiveAsync();
+                    var ackMessage = Encoding.UTF8.GetString(ackResult.Buffer);
+                    if (ackMessage == Broadcast.ACK_MESSAGE)
+                    {
+                        logger.LogInformation($"Received ACK from {ackResult.RemoteEndPoint.Address}:{ackResult.RemoteEndPoint.Port}");
+                        acknowledged = true;
+                    }
+                }
+                catch (SocketException)
+                {
+                    logger.LogInformation("Waiting for ACK...");
+                }
+            }
+        }
+    }
+}
