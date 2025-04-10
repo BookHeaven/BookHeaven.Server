e95b4a2cc04a9ac7f88e1fadb9e3e08c8e8b6f4d
heasheartfire <gagi1995@gmail.com>
2025-04-10 11:49:38 +0200

    feat: Expose endpoint to update profile settings

---
+++ Endpoints/Profiles/ApiUpdateProfileSettings.cs
@@ -0,0 +1,37 @@
+﻿using BookHeaven.Domain.Entities;
+using BookHeaven.Domain.Features.ProfileSettingss;
+using BookHeaven.Server.Abstractions.Api;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHeaven.Server.Endpoints.Profiles;
+
+public static class ApiUpdateProfileSettings
+{
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/profile/settings/update", ApiHandler);
+        }
+        
+        private static async Task<IResult> ApiHandler(
+            [FromBody] ProfileSettings profileSettings,
+            ISender sender,
+            ILogger<ApiGetAllProfiles.Endpoint> logger)
+        {
+            var existingSettings = await sender.Send(new GetProfileSettings.Query(profileSettings.ProfileSettingsId));
+            if (existingSettings.IsFailure)
+            {
+                await sender.Send(new AddProfileSettings.Command(profileSettings));
+            }
+            else
+            {
+                await sender.Send(new UpdateProfileSettings.Command(profileSettings));
+            }
+
+            return Results.Ok();
+        }
+        
+    }
+}
