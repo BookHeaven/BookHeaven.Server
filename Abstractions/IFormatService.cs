a25e21f4034a65089c83d9b586799555ea944acb
heasheartfire <gagi1995@gmail.com>
2025-09-15 23:27:27 +0200

    refactor(IFormatService): remove obsolete methods

--- Abstractions/IFormatService.cs
+++ Abstractions/IFormatService.cs
@@ -4,9 +4,6 @@ namespace BookHeaven.Server.Abstractions;
 
 public interface IFormatService
 {
-	Task DownloadAndStoreCoverAsync(string url, string dest);
-	Task StoreCover(byte[]? image, string dest);
-	Task StoreBook(string? sourcePath, string dest);
 	Task<Guid?> LoadFromFilePath(string path);
 	Task<Guid?> LoadFromFile(IBrowserFile file);
 }
