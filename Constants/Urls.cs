0aeae4e10eeeef655e0b20ea5a68576c9fb46aa6
heasheartfire <gagi1995@gmail.com>
2025-09-15 20:06:04 +0200

    feat(Urls): Add urls for collections

--- Constants/Urls.cs
+++ Constants/Urls.cs
@@ -3,6 +3,9 @@
 public static class Urls
 {
 	public const string Shelf = "/shelf";
+	public const string Collections = "/collections";
+	public const string Collection = $"{Shelf}/collection";
+	public const string NewCollection = $"{Collections}/new";
 	public const string Authors = "/authors";
 	public const string Series = "/series";
 	public const string Settings = "/settings";
@@ -27,5 +30,15 @@ public static class Urls
 	public static string GetProfileUrl(Guid profileId)
 	{
 		return $"{Profiles}/{profileId}";
+	}
+	
+	public static string GetCollectionUrl(Guid collectionId)
+	{
+		return $"{Collection}/{collectionId}";
+	}
+	
+	public static string GetCollectionPageUrl(Guid collectionId)
+	{
+		return $"{Collections}/{collectionId}";
 	}
 }
