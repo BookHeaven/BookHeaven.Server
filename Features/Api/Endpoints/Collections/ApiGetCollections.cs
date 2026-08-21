7bc394530bb64ae0e5b1945b600c498a24234d5f
heasheartfire <gagi1995@gmail.com>
2026-08-21 16:35:26 +0200

    feat(Collections): add API endpoint to retrieve all collections

---
+++ Features/Api/Endpoints/Collections/ApiGetCollections.cs
@@ -0,0 +1,38 @@
+﻿using BookHeaven.Core.Entities.Base;
+using BookHeaven.Core.Features.Books;
+using BookHeaven.Core.Features.Collections;
+using BookHeaven.Server.Features.Api.Abstractions;
+
+namespace BookHeaven.Server.Features.Api.Endpoints.Collections;
+
+public static class ApiGetCollections
+{
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/collections", ApiHandler)
+                .WithName("GetCollections")
+                .WithTags("Collections")
+                .Produces<List<Collection>>()
+                .ProducesProblem(StatusCodes.Status500InternalServerError)
+                .WithSummary("Get list of collections.")
+                .WithDescription("Retrieves a list of all collections.");
+        }
+        
+        private static async Task<IResult> ApiHandler(
+            ISender sender,
+            ILogger<Endpoint> logger)
+        {
+            var getCollections = await sender.Send(new GetAllCollections.Query());
+            
+            if (getCollections.IsFailure)
+            {
+                logger.LogError(getCollections.Error.Description);
+                return Results.Problem(getCollections.Error.Description);
+            }
+            
+            return Results.Ok(getCollections.Value);
+        }
+    }
+}
